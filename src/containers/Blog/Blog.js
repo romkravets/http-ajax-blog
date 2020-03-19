5c1cd26837c09b956a267d8ab3e864227467e76a
romkravets <romann.kravets@gmail.com>
2020-03-19 17:17:00 +0200

    Handling Errors Locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount () {
@@ -26,6 +27,9 @@ class Blog extends Component {
                 this.setState({posts: updatePosts});
                 console.log(response);
             })
+            .catch(error => {
+                this.setState({error: true})
+            })
     }
 
     postSelectedHandler = (id) => {
@@ -33,13 +37,16 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}/>
-        });
+        let posts = <p>Something wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                            key={post.id}
+                            title={post.title}
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
         return (
             <div>
                 <section className="Posts">
